1b119d93ac9ef272008b690d73d12f1f87901d43
Mikko Kivelä <6763161+bolozna@users.noreply.github.com>
2013-03-30 15:59:22 +0000

    Edited net.py and added tests.

--- net.py
+++ net.py
@@ -1,4 +1,4 @@
-
+import networkx
 
 class MultisliceNetwork(object):
     """
@@ -43,24 +43,59 @@ class MultisliceNetwork(object):
 
         #should keep table of degs and strenghts
         
-    def _get_link(self,link):
-        #get it first from the net
+    def _link_to_nodes(self,link):
+        """Returns the link as tuple of nodes in the graph representing
+        the multislice structure. I.e. when given (i,j,s_1,r_1, ... ,s_d,r_d)
+        (i,s_1,...,s_d),(j,r_1,...,r_d) is returned.
+        """
+        return (item[0],)+item[2::2],(item[1],)+item[3::2]
 
-        #then look at the couplings
-        pass
+    def _get_link(self,link):
+        """Return link weight or 0 if no link.
+        
+        This is a private method, so no sanity checks on the parameters are
+        done at this point.
+
+        Paramters
+        ---------
+        link(tuple) : (i,j,s_1,r_1, ... ,s_d,r_d)
+        """
+        node1,node2=self._link_to_nodes(link)
+        return self.net[node1][node2]['weight']
+
+    def _set_link(self,link,value):
+        node1,node2=self._link_to_nodes(link)
+        self.net[node1][node2]['weight']=value
+
+    def _get_degree(self,node, dims):
+        if dims==None:
+            return self.net.degree(node)
+        else:
+            return len(list(self._iter_neighbors(node,dims)))
+    def _iter_neighbors(self,node,dims):
+        if dims==None:
+            for neigh in self.net[node]:
+                yield neigh
+        else:
+            for neigh in self.net[node]:
+                if all(map(lambda i:dims[i]==None or neigh[i]==dims[i], range(len(dims)))):
+                    yield neigh
 
     def __getitem__(self,item):
         """
-
-        dimension=2
+        dimensions=2
         i,s     = node i at slice s
         i,j,s   = link i,j at slice s = i,j,s,s
         i,j,s,r = link i,j between slices s and r
 
-        i,: = multislice node (=iterator,deg etc)
-        :,:,s,s = view to slice s
+        i,:,s,: = i,s = node i at slice s
+        i,j,s,: = node i at slice s, but only links to j are visible
+        i,:,s,r = node i at slice s, but only links to slide s are visible
 
-        dimension=3
+        i,:,s   = i,:,s,s
+        i,s,:   = i,i,s,:
+
+        dimensions=3
         i,s,x = node i at slice1 and slice2 x
         i,j,s,x = same as i,j,s,s,x,x
         i,j,s,r,x,y = ...
@@ -82,12 +117,10 @@ class MultisliceNetwork(object):
 
     def __setitem__(self,item,val):
         pass
-    
-    def get_couplings(self,dimension):
-        """Returns a view to a network of couplings between nodes and
-        their counterparts in other slices of the given dimension.        
+
+    def __iter__(self):
+        """Iterates over all nodes.
         """
-        pass
 
     def add_slice(self,dimension,s):
         pass
@@ -95,8 +128,6 @@ class MultisliceNetwork(object):
     def add_node(self,node):
         pass
 
-    def neighbors(self,*args):
-        pass
     def deg(self,*args):
         """
         net.deg(i)
@@ -111,15 +142,37 @@ class MultisliceNetwork(object):
         """
         pass
 
-    def set_connection_policy(self,dimension,policy):
+class MultisliceNode(object):
+    def __init__(self,node,mnet,layers=None):
+        """A node in multilice network. 
+
+        ...
+        """
+        self.node=node
+        self.mnet=mnet
+        self.layers=layers
+    def deg(self,*layers):
+        assert len(layers)==0 or len(layers)==self.mnet.dimensions
+        if layers==():
+            layers=self.layers
+        return self.mnet._get_deg(self.node,layers)
+    def str(self,*layers):
+        pass
+    def __iter__(self):
+        for node in self.mnet._iter_neighbors(self.node,self.layers):
+            yield node #maybe should only return the indices that can change?
+    def layers(self,*layers):
+        return MultisliceNode(self.node,self.mnet,layers=layers)
+
+
+class CoupledMultiplexNetwork(MultisliceNetwork):
+    def get_couplings(self,dimension):
+        """Returns a view to a network of couplings between nodes and
+        their counterparts in other slices of the given dimension.        
+        """
         pass
 
-class MultisliceNode(object):
-    def __init__(self,mnet,selected_dimension=None):
-        """A node in multilice network. If dimension is
-        given, then only other nodes in that dimension are
-        considered as neighbors of this node.
-        """
+    def set_connection_policy(self,dimension,policy):
         pass
 
 class FlatMultisliceNetworkView(object):
@@ -141,6 +194,21 @@ class ModularityMultisliceNetworkView(MultisliceNetwork):
     def __init__(self,mnet,alpha=1.0,omega=1.0):
         self.mnet=mnet
         #precalc ms,kis,u
+        #copy mnet to self
+
+    def _get_link(self,link):
+        v=self.mnet[item]
+        if v>0:
+            if item[0]!=item[1]: #its inside slice
+                kis=self.mnet.str((item[0],)+item[2::2])
+                kjs=self.mnet.str((item[1],)+item[3::2])
+                ms=None
+                v-alpha*kis*kjs/float(2.0*ms)
+            else:
+                return omega*v
+        else:
+            return v
+    """
     def __getitem__(self,item):
         if len(item)==2*self.mnet.dimensions:
             v=self.mnet[item]
@@ -156,7 +224,7 @@ class ModularityMultisliceNetworkView(MultisliceNetwork):
                 return v
         else:
             pass
-    
+    """
 
 try:
     import networkx
@@ -164,3 +232,9 @@ try:
         pass
 except ImportError:
     pass
+
+
+
+if __name__ == '__main__':
+    import tests.net_test
+    tests.net_test.test_net()
