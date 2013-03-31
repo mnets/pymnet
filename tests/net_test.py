9f6a13bd1f25b2d41e9c8fdcb647d7aea3558546
Mikko Kivelä <6763161+bolozna@users.noreply.github.com>
2013-03-31 18:33:09 +0100

    Implemented getting and setting for Multislice networks. Setting for shortened keys still unimplemented.

--- tests/net_test.py
+++ tests/net_test.py
@@ -18,9 +18,15 @@ class TestNet(unittest.TestCase):
         net[1,2,'b']=1
         net[1,3,'b']=1
 
+        self.assertEqual(net[1,2,'a','a'],1)
+
+        self.assertEqual(set(net[1,'a']),set([(1,'b'),(2,'a')]))
+        self.assertEqual(set(net[1,:,'a',:]),set([(1,'b'),(2,'a')]))
+        self.assertEqual(list(net[1,:,'a','a']),[(2,'a')])
+        self.assertEqual(list(net[1,1,'a',:]),[(1,'b')])
                 
     def test_simple_couplings_mnet(self):
-        testnet=net.MultisliceNetwork()
+        testnet=net.MultisliceNetwork(dimensions=2)
         testnet[1,1,'a','b']=1
         testnet[2,2,'a','b']=1
         testnet[3,3,'a','b']=1
