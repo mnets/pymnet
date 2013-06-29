481c24e1ee15bef29ad3405bdeea81678ca23fc7
Mikko Kivela <bolozna@gmail.com>
2013-06-29 12:16:28 +0100

    New clustering coefficients, and models to test them. Includes all the tests etc done during my pass of the tensor draft.

--- tests/cc_test.py
+++ tests/cc_test.py
@@ -102,6 +102,10 @@ class TestNet(unittest.TestCase):
 
     def test_unweighted_consistency(self,net):
         anet=transforms.aggregate(net,1)
+        onet=transforms.overlay_network(net)
+        wmax=max(map(lambda x:x[2],anet.edges))
+        owmax=max(map(lambda x:x[2],onet.edges))
+        b=len(net.slices[1])
 
         if cc.cc_cycle_vector_bf(net,1,1)!=cc.cc_cycle_vector_adj(net,1,1):
             print list(anet.edges)
@@ -132,8 +136,6 @@ class TestNet(unittest.TestCase):
 
         self.assertAlmostEqual(cc.gcc_alternating_walks_seplayers(net,w1=0.3,w2=0.3,w3=0.3),cc.gcc_alternating_walks_seplayers_adj(net,w1=0.3,w2=0.3,w3=0.3))
 
-        wmax=max(map(lambda x:x[2],anet.edges))
-        b=len(net.slices[1])
         for supernode in net.slices[0]:
             if abs(cc.sncc_alternating_walks(net,supernode,a=0.5,b=0.5)-wmax/float(b)*cc.cc_zhang(anet,supernode))>10**-6:
                 print wmax,b,cc.sncc_alternating_walks(net,supernode,a=0.5,b=0.5),cc.cc_zhang(anet,supernode)
@@ -141,6 +143,66 @@ class TestNet(unittest.TestCase):
                 print list(anet.edges)
                 print list(net.edges)
             self.assertAlmostEqual(cc.sncc_alternating_walks(net,supernode,a=0.5,b=0.5),wmax/float(b)*cc.cc_zhang(anet,supernode))
+
+        #global cc
+        tga=0
+        tgaw=0
+        tgmoreno=0
+        for node in net:
+            for i,j in itertools.combinations(anet[node],2):
+                tga+=2*anet[node][i]*anet[node][j]*anet[i][j]
+            for l in net.slices[1]:
+                aaa,aacac,acaac,acaca,acacac, afa,afcac,acfac,acfca,acfcac=cc.cc_cycle_vector_bf(net,node,l)
+                tgaw+=aaa+aacac+acaac+acaca+acacac
+                tgmoreno+=aaa+2*aacac+2*acaac+acaca+2*acacac
+        print "gcc_t",tga,tgaw,tgmoreno
+        """
+        print "gcc",cc.gcc_contraction_m(net),cc.gcc_contraction_m_ct(net),cc.gcc_super_graph(net),cc.gcc_contraction_o2(net),owmax*cc.gcc_zhang(onet)/float(b),cc.gcc_contraction_o(net)
+        """
+
+    def test_unweighted_consistency_mslice(self,net):
+        anet=transforms.aggregate(net,1)
+        onet=transforms.overlay_network(net)
+        wmax=max(map(lambda x:x[2],anet.edges))
+        owmax=max(map(lambda x:x[2],onet.edges))
+        b=len(net.slices[1])
+
+        self.assertAlmostEqual(cc.gcc_contraction_m(net),cc.gcc_contraction_m_ct(net))
+        self.assertAlmostEqual(cc.gcc_contraction_m_ct(net),cc.gcc_contraction_m_full(net))
+        #self.assertAlmostEqual(cc.gcc_contraction_m_full(net),cc.gcc_super_graph(net))
+
+        self.assertAlmostEqual(cc.gcc_contraction_o2(net),owmax*cc.gcc_zhang(onet)/float(b))
+        self.assertAlmostEqual(cc.gcc_contraction_o2(net),cc.gcc_contraction_o_full(net))
+
+        print "gcc_0,02,Oz,b,wmax:",cc.gcc_contraction_o(net),cc.gcc_contraction_o2(net),cc.gcc_zhang(onet),b,owmax
+
+        #print "gcc",cc.gcc_contraction_m(net),cc.gcc_contraction_m_ct(net),cc.gcc_contraction_m_full(net),cc.gcc_super_graph(net),cc.gcc_contraction_o2(net),owmax*cc.gcc_zhang(onet)/float(b),cc.gcc_contraction_o(net)
+
+
+    def test_unweighted_consistency_mslice_er(self):
+        net=models.er_multislice(10,5,0.5,randomWeights=True)
+        self.test_unweighted_consistency_mslice(net)
+
+        net=models.er_multislice(10,5,0.1,randomWeights=True)
+        self.test_unweighted_consistency_mslice(net)
+
+    def test_normalization_full_mslice(self):
+        net=models.full_multislice(5,5)
+        self.assertEqual(cc.gcc_contraction_m(net),1.0)
+        self.assertEqual(cc.gcc_contraction_m_ct(net),1.0)
+        self.assertEqual(cc.gcc_contraction_m_full(net),1.0)
+        self.assertEqual(cc.gcc_super_graph(net),1.0)
+
+        self.assertEqual(cc.gcc_contraction_o2(net),1.0)
+        self.assertEqual(cc.gcc_contraction_o_full(net),1.0)
+
+        onet=transforms.overlay_network(net)
+        owmax=max(map(lambda x:x[2],onet.edges))
+        b=len(net.slices[1])
+        self.assertEqual(owmax*cc.gcc_zhang(onet)/float(b),1.0)
+        self.assertEqual(owmax,b)
+
+        self.assertNotEqual(cc.gcc_contraction_o(net),1.0)
 
 
     def test_unweighted_consistency_er(self):
@@ -158,6 +220,20 @@ class TestNet(unittest.TestCase):
         net[5, 8, 0, 0]=1  
         self.test_unweighted_consistency(net)
 
+        net=pymnet.net.CoupledMultiplexNetwork([('categorical',1.0)])
+        #net.add_node(1,0)
+        net.add_node(1,1)
+        net.add_node(2,1)
+        net.add_node(3,1)
+        net.add_node(4,1)
+        net[1,2,1,1]=1
+        net[1,3,1,1]=1
+        net[2,3,1,1]=1
+        self.test_unweighted_consistency(net)
+
+        #net=models.full_multislice(10,5)
+        #self.test_unweighted_consistency(net)
+
 def test_net():
     suite = unittest.TestSuite()    
     suite.addTest(TestNet("test_unweighted_flat_triangle"))
@@ -165,7 +241,8 @@ def test_net():
     suite.addTest(TestNet("test_unweighted_mplex_triangle"))
     suite.addTest(TestNet("test_unweighted_mplex_simple"))
     suite.addTest(TestNet("test_unweighted_consistency_er"))
-
+    suite.addTest(TestNet("test_unweighted_consistency_mslice_er"))
+    suite.addTest(TestNet("test_normalization_full_mslice"))
     unittest.TextTestRunner().run(suite) 
 
 if __name__ == '__main__':
