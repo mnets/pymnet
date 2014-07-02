b28ebabbcb1b714981f843563f1a39a60586f229
Mikko Kivelä <6763161+bolozna@users.noreply.github.com>
2014-07-02 10:17:47 +0100

    Added a module for wrapping networkx objects. Currently read only view of monoplex networks.

--- pymnet/__init__.py
+++ pymnet/__init__.py
@@ -5,3 +5,8 @@ from io import read_ucinet
 from diagnostics import degs,density,multiplex_degs,multiplex_density
 from cc import   lcc,cc_zhang,gcc_zhang,cc_onnela,cc_barrat,cc_barrett,cc_sequence,lcc_aw,avg_lcc_aw,gcc_aw,sncc_aw,elementary_cycles,lcc_brodka
 from visuals import webplot
+
+try:
+    import nxwrap as nx
+except ImportError: #in case networkx is not installed
+    pass
