47a3e478059e0590baab52c73716a40b91732e46
Mikko Kivelä <6763161+bolozna@users.noreply.github.com>
2014-10-27 15:32:14 +0000

    Fixed a bug in cc_barrett_optimized method.

--- pymnet/cc.py
+++ pymnet/cc.py
@@ -177,7 +177,7 @@ def cc_barrett_optimized(net,node,anet,undefReturn=0.0):
                 if net.fullyInterconnected:
                     layers=net.slices[1]
                 else:
-                    layers=itertools.imap(lambda x:x[0],net._nodeToLayers[h])
+                    layers=net._nodeToLayers[h] #itertools.imap(lambda x:x[0],net._nodeToLayers[h])
                 for layer in layers:
                     m+=max(net[node,h,layer],net[j,h,layer])
                 den+=anet[node,j]*m
