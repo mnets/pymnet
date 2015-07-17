229a71e9e996fb28e23135a10f45410226f8c048
Mikko Kivelä <6763161+bolozna@users.noreply.github.com>
2015-07-17 10:51:18 +0100

    Added some code for Dijkstra's algorithm. Fixed a bug in nxwrap where a correct exception was not thrown.

--- pymnet/tests/diagnostics_test.py
+++ pymnet/tests/diagnostics_test.py
@@ -2,7 +2,7 @@ import unittest
 import sys
 sys.path.append("../../")
 
-from pymnet import net,diagnostics
+from pymnet import net,diagnostics,models,nx
 
 
 class TestDiagnostics(unittest.TestCase):    
@@ -90,12 +90,73 @@ class TestDiagnostics(unittest.TestCase):
         self.assertEqual(diagnostics.degs(n,degstype="nodes"),{(1,1):1,(2,1):2,(3,1):2,(4,1):1,(1,3):1,(2,3):2,(3,3):2,(4,3):1})
 
 
+    def test_dijkstra_monoplex(self):
+        n=net.MultilayerNetwork(aspects=0)
+        n[1,2]=1
+        n[1,3]=1
+        n[2,3]=1
+        n[2,4]=1
+        n[3,4]=1
+        n[3,5]=1
+
+        d,f=diagnostics.dijkstra(n,[1])
+
+        ftrue=net.MultilayerNetwork(aspects=0,fullyInterconnected=False,directed=True,noEdge=-1)
+        ftrue[1,1]=0;ftrue[1,2]=1;ftrue[1,3]=1;ftrue[2,4]=1;ftrue[3,4]=1;ftrue[3,5]=1
+        self.assertEqual(d,{1:0,2:1,3:1,4:2,5:2})
+        self.assertEqual(f,ftrue)
+
+        d,f=diagnostics.dijkstra(n,[1,2])
+        ftrue=net.MultilayerNetwork(aspects=0,fullyInterconnected=False,directed=True,noEdge=-1)
+        ftrue[1,1]=0;ftrue[2,2]=0;ftrue[1,3]=1;ftrue[2,3]=1;ftrue[2,4]=1;ftrue[3,5]=1
+        self.assertEqual(d,{1:0,2:0,3:1,4:1,5:2})
+        self.assertEqual(f,ftrue)
+
+    def test_dijkstra_monoplex_compare(self):
+        n=models.er(100,0.1)
+        d,f=diagnostics.dijkstra(n,[1])
+        
+        self.assertEqual(d, nx.shortest_path_length(n,1))
+        
+    def test_dijkstra_multilayer_two_aspect(self):
+        n=net.MultilayerNetwork(aspects=2,directed=True)
+        n[1,'a',1][2,'a',2]=1
+        n[2,'a',2][3,'a',3]=1
+        n[3,'a',3][4,'a',4]=1
+        n[1,'b',1.5][3,'b',2.5]=1
+
+        n[1,'a',1][1,'b',1.25]=0.25
+        n[1,'b',1.5][1,'a',1.75]=0.25
+        n[3,'b',2.5][3,'a',2.75]=0.25
+
+        n[1,'a',1.75][1,'a',2]=0.25
+        n[1,'b',1.25][1,'b',1.5]=0.25
+        n[3,'a',2.75][3,'a',3]=0.25
+
+
+        d,f=diagnostics.dijkstra(n,[(1,'a',1)])
+        for nl,dist in d.iteritems():            
+            self.assertEqual(d[nl],nl[2]-1)
+
+        d,f=diagnostics.dijkstra_mlayer_prune(n,[(1,None,None)],aaspects=[1,2])
+        self.assertEqual(d[(1,)],0)
+        self.assertEqual(d[(2,)],1)
+        self.assertEqual(d[(3,)],1)
+        self.assertEqual(d[(4,)],2.5)
+        ftrue=net.MultilayerNetwork(aspects=2,fullyInterconnected=False,directed=True,noEdge=-1)
+        ftrue[1,'a',1][1,'a',1]=0; ftrue[1,'b',1.5][1,'b',1.5]=0;ftrue[1,'b',1.25][1,'b',1.25]=0; ftrue[1,'a',1.75][1,'a',1.75];ftrue[1,'a',2][1,'a',2]
+
+        print d,list(f.edges)
+
 def test_diagnostics():
     suite = unittest.TestSuite()    
     suite.addTest(TestDiagnostics("test_monoplex_density_degs_mnet"))
     suite.addTest(TestDiagnostics("test_multiplex_density_degs_mnet"))
     suite.addTest(TestDiagnostics("test_multilayer_degs_multilayernet"))
     suite.addTest(TestDiagnostics("test_multilayer_degs_mplexnet"))
+    suite.addTest(TestDiagnostics("test_dijkstra_monoplex"))
+    suite.addTest(TestDiagnostics("test_dijkstra_monoplex_compare"))
+    suite.addTest(TestDiagnostics("test_dijkstra_multilayer_two_aspect"))
 
     unittest.TextTestRunner().run(suite) 
 
