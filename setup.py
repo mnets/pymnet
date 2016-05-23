a8c57198e46be4d17f25ffe86d4c386082f8c367
Luiz Irber <luiz.irber@gmail.com>
2016-05-22 17:49:21 -0700

    Add simple setup.py and tox support

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name="pymnet",
+    version="0.1",
+    description="Library for analysing multilayer networks",
+    url="https://bitbucket.org/bolozna/multilayer-networks-library",
+    author="Mikko Kivelä",
+    author_email="mikko.kivela@iki.fi",
+    py_modules=["pymnet", "pymnet.tests"]
+)
