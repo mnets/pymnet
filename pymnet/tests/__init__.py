3cce24909a578bb956f7ee656f07b14bc5a55cee
Mikko Kivelä <6763161+bolozna@users.noreply.github.com>
2016-05-24 15:41:19 +0100

    Added unit tests for visualizations. Tests produce figures under the test dir, and only way for them to fail is if exceptions are thrown.

--- pymnet/tests/__init__.py
+++ pymnet/tests/__init__.py
@@ -4,6 +4,7 @@ from diagnostics_test import test_diagnostics
 from io_test import test_io
 from models_test import test_models
 from transforms_test import test_transforms
+from visuals_test import test_visuals
 
 def test_all():
     test_net()
@@ -12,3 +13,4 @@ def test_all():
     test_io()
     test_models()
     test_transforms()
+    test_visuals()
