972942af43d3435ce25926edbf21a30a2ad1af30
Mikko Kivelä <6763161+bolozna@users.noreply.github.com>
2017-02-08 14:37:13 +0200

    Refactoring visuals module. It is now a package.

---
+++ pymnet/visuals/drawassigners.py
@@ -0,0 +1,164 @@
+import math
+
+class PropertyAssigner(object):
+    rules=set(["order","name","f"])
+    def __init__(self,propDict,propRule,defaultProp,net):
+        self.propDict=propDict
+        self.propRule=propRule
+        self.defaultProp=defaultProp
+        self.net=net
+
+    def _get_from_property_dict(self,item):
+        if item in self.propDict:
+            return self.propDict[item]
+        else:
+            return None
+
+    def __getitem__(self,item):
+        pdictval=self._get_from_property_dict(item)
+        if pdictval!=None:
+            return pdictval
+        elif len(self.propRule)>0:
+            assert "rule" in self.propRule
+            if self.propRule["rule"] in self.rules:
+                return self.apply_modify_rules(self.get_by_rule(item,self.propRule["rule"]))
+            else:
+                raise Exception("Unknown rule: "+str(self.propRule["rule"]))
+        else:
+            return self.defaultProp
+
+    def get_by_rule(self,item,rule):
+        if rule=="order":
+            assert "sequence" in self.propRule
+            if hasattr(self,"i"):
+                self.i+=1
+            else:
+                self.i=0
+            return self.propRule["sequence"][self.i%len(self.propRule["sequence"])]
+        elif rule=="name":
+            return item
+
+    def apply_modify_rules(self,item):
+        if "f" in self.propRule and self.propRule["rule"]!="f":
+            item=self.propRule["f"](item)
+        if "scaleby" in self.propRule:
+            item=item*self.propRule["scaleby"]
+        if "colormap" in self.propRule:
+            item=matplotlib.cm.get_cmap(self.propRule["colormap"])(item)
+        return item
+
+class LayerPropertyAssigner(PropertyAssigner):
+    pass
+
+class LayerColorAssigner(LayerPropertyAssigner):
+    pass
+class LayerAlphaAssigner(LayerPropertyAssigner):
+    pass
+class LayerLabelAssigner(LayerPropertyAssigner):
+    pass
+class LayerLabelLocAssigner(LayerPropertyAssigner):
+    pass
+class LayerOrderAssigner(LayerPropertyAssigner):
+    pass
+class LayerLabelSizeAssigner(LayerPropertyAssigner):
+    pass
+class LayerLabelColorAssigner(LayerPropertyAssigner):
+    pass
+class LayerLabelStyleAssigner(LayerPropertyAssigner):
+    pass
+class LayerLabelAlphaAssigner(LayerPropertyAssigner):
+    pass
+
+
+class NodePropertyAssigner(PropertyAssigner):
+    rules=PropertyAssigner.rules.union(set(["degree"]))
+    def get_by_rule(self,item,rule):
+        if rule=="degree":
+            return self.net[item].deg()
+        return super(NodePropertyAssigner,self).get_by_rule(item,rule)
+
+class NodeLabelSizeAssigner(NodePropertyAssigner):
+    pass
+class NodeLabelColorAssigner(NodePropertyAssigner):
+    pass
+class NodeLabelStyleAssigner(NodePropertyAssigner):
+    pass
+class NodeLabelAlphaAssigner(NodePropertyAssigner):
+    pass
+
+class NodeLabelAssigner(NodePropertyAssigner):
+    rules=NodePropertyAssigner.rules.union(set(["nodename"]))
+    def get_by_rule(self,item,rule):
+        if rule=="nodename":
+            return item[0]
+        return super(NodeLabelAssigner,self).get_by_rule(item,rule)
+
+class NodeColorAssigner(NodePropertyAssigner):
+    rules=NodePropertyAssigner.rules-set(["name"])
+
+class NodeSizeAssigner(NodePropertyAssigner):
+    rules=NodePropertyAssigner.rules.union(set(["scaled"]))-set(["name"])
+    def get_by_rule(self,item,rule):
+        if rule=="scaled":
+            coeff=self.propRule["scalecoeff"] if "scalecoeff" in self.propRule else 1.0
+            n=len(self.net)     
+            return coeff/float(math.sqrt(n))
+        return super(NodeSizeAssigner,self).get_by_rule(item,rule)
+
+    def apply_modify_rules(self,item):
+        if "propscale" in self.propRule:
+            coeff=self.propRule["propscale"]
+            n=len(self.net)     
+            item=item*coeff/float(math.sqrt(n))
+        return super(NodeSizeAssigner,self).apply_modify_rules(item)
+
+
+#nodes todo: marker
+
+class EdgePropertyAssigner(PropertyAssigner):
+    rules=NodePropertyAssigner.rules.union(set(["edgetype","edgeweight"]))
+
+    def _get_from_property_dict(self,item):
+        """Return the edge property from the property dict given by the user.
+
+        For directed networks this is same as the parent classes method. For 
+        undirected networks both directions for edges are accepted.
+        """
+        if self.net.directed:
+            return super(EdgePropertyAssigner,self)._get_from_property_dict(item)
+        else:
+            if item in self.propDict:
+                return self.propDict[item]
+            else:
+                try:
+                    item=(item[1],item[0])
+                    if item in self.propDict:
+                        return self.propDict[item]
+                except Exception:
+                    return None                    
+            return None
+
+    def get_by_rule(self,item,rule):
+        if rule=="edgetype":
+            if "intra" in self.propRule and item[0][1]==item[1][1]:
+                return self.propRule["intra"]
+            elif "inter" in self.propRule and item[0][1]!=item[1][1]:
+                return self.propRule["inter"]
+        elif rule=="edgeweight":
+            return self.net[item[0]][item[1]]
+
+class EdgeWidthAssigner(EdgePropertyAssigner):
+    pass
+
+class EdgeColorAssigner(EdgePropertyAssigner):
+    pass
+
+class EdgeStyleAssigner(EdgePropertyAssigner):
+    pass
+
+class EdgeAlphaAssigner(EdgePropertyAssigner):
+    pass
+
+class EdgeZAssigner(EdgePropertyAssigner):
+    pass
+
---
+++ pymnet/visuals/drawnet.py
@@ -0,0 +1,103 @@
+class NetFigure(object):    
+    def __init__(self,figsize=None,layergap=1,eps=0.001,padding=0.05,azim=-51,elev=22,show=False,camera_dist=None,autoscale=True):
+        self.nodes=[]
+        self.layers=[]
+        self.edges=[]
+
+        self.padding=padding
+        self.eps=eps        
+        self.layergap=layergap
+        self.figsize=figsize
+        self.azim=azim
+        self.elev=elev
+        self.show=show
+        self.camera_dist=camera_dist
+        self.autoscale=autoscale
+
+    def normalize_coords(self):
+        maxx,maxy,minx,miny=float("-inf"),float("-inf"),float("inf"),float("inf")
+        for node in self.nodes:
+            if maxx<node.x+node.size/2.: maxx=node.x+node.size/2.
+            if maxy<node.y+node.size/2.: maxy=node.y+node.size/2.
+            if minx>node.x-node.size/2.: minx=node.x-node.size/2.
+            if miny>node.y-node.size/2.: miny=node.y-node.size/2.
+
+        xtrans=lambda x:(x-minx+self.padding)/float(maxx-minx+2*self.padding)
+        ytrans=lambda y:(y-miny+self.padding)/float(maxy-miny+2*self.padding)
+
+        for node in self.nodes:
+            node.x=xtrans(node.x)
+            node.y=ytrans(node.y)
+
+
+    def draw_elements(self):
+        for i,layer in enumerate(self.layers):
+            layer.z=i*self.layergap
+            if layer.alpha!=0:
+                layer.draw()
+
+        for node in self.nodes:
+            if node.size>0:
+                node.draw()
+
+        for edge in self.edges:
+            edge.draw()
+
+
+    def draw(self):
+        #Override this method
+        raise NotImplemented()
+
+    def register_layer(self,layer):
+        self.layers.insert(0,layer) #First to top
+    def register_node(self,node):
+        self.nodes.append(node)
+    def register_edge(self,edge):
+        self.edges.append(edge)
+
+class Node(object):
+    def __init__(self,layer,x,y,label=None,size=0.04,color="black",labelArgs={}):
+        self.x,self.y,self.size,self.color,self.label=x,y,size,color,label
+        self.layer=layer
+        self.net=layer.net
+        self.label=label
+        self.labelArgs=labelArgs 
+
+        self.net.register_node(self)        
+
+    def draw(self):
+        #Override this method
+        raise NotImplemented()
+
+
+class Layer(object):
+    def __init__(self,net,color="gray",alpha=0.3,shape="rectangle",label=None,labelloc=(1,1),labelArgs={}):
+        assert shape in ["rectangle","circle"]
+        self.shape=shape
+        self.color=color
+        self.alpha=alpha
+        self.label=label
+        self.labelloc=labelloc
+        self.labelArgs=labelArgs 
+        self.z=None
+        self.net=net
+        self.net.register_layer(self)
+
+    def draw(self):
+        #Override this method
+        raise NotImplemented()
+
+
+class Edge(object):
+    def __init__(self,node1,node2,color="gray",width=1.0,directed=False,style="-",z=0,alpha=1):
+        self.node1=node1
+        self.node2=node2
+        self.net=node1.net
+        assert 0<=z<=1
+        self.z=z
+        assert 0<=alpha<=1
+        self.alpha=alpha
+
+        self.net.register_edge(self)
+
+        self.color,self.width,self.directed,self.style=color,width,directed,style
