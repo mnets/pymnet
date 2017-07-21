dfff7887ad4675584ced3f3c7555c33b24d0f74a
Tarmo Nurmi <tarmo.nurmi@gmail.com>
2017-07-21 18:50:42 +0300

    Updated check function selection preamble for dumb enumeration to be identical to esu

--- pymnet/sampling/dumb.py
+++ pymnet/sampling/dumb.py
@@ -28,21 +28,43 @@ def dumbEnumeration(network,resultlist,sizes=None,intersections=None,nnodes=None
     """
     check_function = None
     assert (sizes != None and intersections != None) or (nnodes != None and nlayers != None), "Please provide either sizes and intersections or nnodes and nlayers"
-    if sizes != None: # Work in progress
-        assert intersections != None and nnodes == None and nlayers == None, "Please provide intersections when providing sizes, and not nnodes or nlayers"
-        req_nodelist_len, req_layerlist_len = default_calculate_required_lengths(sizes,intersections)
-        check_function = lambda x,y,z: default_check_reqs(x,y,z,sizes,intersections,req_nodelist_len,req_layerlist_len)
-    if nnodes != None:
-        assert nlayers != None and sizes == None and intersections == None, "Please provide nlayers when providing nnodes, and not sizes or intersections"
+    if custom_check_function != None:
+        assert nnodes != None and nlayers != None, "Please provide nnodes and nlayers when using a custom check function"
+        req_nodelist_len = nnodes
+        req_layerlist_len = nlayers
+        check_function = custom_check_function
+    if sizes != None and intersections != None and check_function == None:
+        if isinstance(intersections,list):
+            if None in intersections:
+                assert nnodes != None, "Please provide nnodes if including Nones in intersections"
+                req_nodelist_len = nnodes
+                req_layerlist_len = len(sizes)
+            else:
+                if intersection_type == "strict":
+                    assert nnodes == None and nlayers == None, "You cannot provide both sizes and intersections and nnodes and nlayers, if intersections is a list"
+                    req_nodelist_len, req_layerlist_len = default_calculate_required_lengths(sizes,intersections)
+                elif intersection_type == "less_or_equal":
+                    assert nnodes != None and nlayers == None, "please provide nnodes (and not nlayers) if using less_or_equal intersection type"
+                    req_nodelist_len = nnodes
+                    req_layerlist_len = len(sizes)
+            check_function = lambda x,y,z: default_check_reqs(x,y,z,sizes,intersections,req_nodelist_len,req_layerlist_len,intersection_type)
+        elif isinstance(intersections,int):
+            assert intersections >= 0, "Please provide nonnegative common intersection size"
+            assert nnodes != None and nlayers == None, "When requiring only common intersection size, please provide nnodes (and not nlayers)"
+            req_nodelist_len = nnodes
+            req_layerlist_len = len(sizes)
+            intersections_as_list = [None]*(2**len(sizes)-len(sizes)-1)
+            intersections_as_list[-1] = intersections
+            check_function = lambda x,y,z: default_check_reqs(x,y,z,sizes,intersections_as_list,req_nodelist_len,req_layerlist_len,intersection_type)
+            #check_function = lambda x,y,z: check_only_common_intersection(x,y,z,intersections,intersection_type)
+    if nnodes != None and nlayers != None and check_function == None:
+        assert sizes == None and intersections == None, "You cannot provide both sizes and intersections and nnodes and nlayers, if intersections is a list"
         req_nodelist_len = nnodes
         req_layerlist_len = nlayers
         assert isinstance(req_nodelist_len,int) and isinstance(req_layerlist_len,int), "Non-integer nnodes or nlayers"
         assert req_nodelist_len > 0 and req_layerlist_len > 0, "Nonpositive nnodes or nlayers"
         check_function = relaxed_check_reqs
-    if custom_check_function != None:
-        assert nnodes != None and nlayers != None, "Please provide nnodes and nlayers when using a custom check function"
-        check_function = custom_check_function
-    assert check_function != None, "Please specify a method of subgraph validity checking"
+    assert check_function != None, "Please specify a valid combination of parameters to determine method of subgraph validity checking"
     
     for nodelist in list(itertools.combinations(list(network.iter_nodes()),req_nodelist_len)):
         for layerlist in list(itertools.combinations(list(network.iter_layers()),req_layerlist_len)):
