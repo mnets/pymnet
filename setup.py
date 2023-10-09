31745425753c034ea12a6fc87af3accd5b625529
Sebastian Dalleiger <61188054+sdall@users.noreply.github.com>
2023-10-09 09:38:44 +0200

    Update URL from BitBucket to GitHub

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name="pymnet",
     version="0.1",
     description="Library for analysing multilayer networks",
-    url="https://bitbucket.org/bolozna/multilayer-networks-library",
+    url="https://github.com/bolozna/Multilayer-networks-library",
     author="Mikko Kivelä",
     author_email="mikko.kivela@iki.fi",
     packages=["pymnet", "pymnet.tests","pymnet.visuals","pymnet.visuals.drawbackends","pymnet.isomorphisms","pymnet.graphlets","pymnet.sampling"]
