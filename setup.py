ab4d900c36e9ddbe70c841d436555be0e0f7fe6e
Arash Badie-Modiri <arashbm@gmail.com>
2023-10-31 02:05:24 +0100

    add basic pyproject.toml file

--- setup.py
+++ setup.py
@@ -4,12 +4,4 @@
 from setuptools import setup
 
 
-setup(
-    name="pymnet",
-    version="0.1",
-    description="Library for analysing multilayer networks",
-    url="https://bitbucket.org/bolozna/multilayer-networks-library",
-    author="Mikko Kivelä",
-    author_email="mikko.kivela@iki.fi",
-    packages=["pymnet", "pymnet.tests","pymnet.visuals","pymnet.visuals.drawbackends","pymnet.isomorphisms","pymnet.graphlets","pymnet.sampling"]
-)
+setup()
