ea41d90d13941f0c649236009a676cf671c7de28
dataspider <corinna.coupette@gmail.com>
2024-07-14 17:12:17 +0200

    ...or not

--- pymnet/test_tutorials/__init__.py
+++
@@ -1,6 +0,0 @@
-from .tutorial_test import test_tutorials
-
-def test_all():
-    codes = []
-    codes.append(test_tutorials())
-    return all(codes)
--- pymnet/test_tutorials/__main__.py
+++
@@ -1,5 +0,0 @@
-import sys
-
-from . import test_all
-
-sys.exit(not test_all())
